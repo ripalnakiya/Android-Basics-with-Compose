5db06c91a2e06abc957df43658351350862b9da1
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2026-09-13 00:09:23 +0530

    06 Business Card
    functional app

--- 06_BusinessCard/app/src/main/java/com/ripalnakiya/businesscard/MainActivity.kt
+++ 06_BusinessCard/app/src/main/java/com/ripalnakiya/businesscard/MainActivity.kt
@@ -4,14 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ripalnakiya.businesscard.ui.theme.BlueExtraDark
 import com.ripalnakiya.businesscard.ui.theme.BusinessCardTheme
+import com.ripalnakiya.businesscard.ui.theme.GreenDark
+import com.ripalnakiya.businesscard.ui.theme.GreenLight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BusinessCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    BusinessCard(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +47,75 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BusinessCard(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize()
+            .background(GreenLight),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        UserInfo()
+
+        Spacer(Modifier.size(240.dp))
+
+        ContactInfo()
+    }
+}
+
+@Composable
+fun UserInfo(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier
+                .background(BlueExtraDark)
+                .size(120.dp),
+        )
+        Text(
+            text = "Ripal Nakiya",
+            fontSize = 40.sp,
+            modifier = Modifier.padding(top = 12.dp, bottom = 12.dp)
+        )
+        Text(
+            text = "Android Developer",
+            color = GreenDark,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun ContactInfo(modifier: Modifier = Modifier) {
+    Column(modifier.padding(bottom = 80.dp)) {
+        ContactRow(painterResource(R.drawable.ic_call), "0123456789")
+        ContactRow(painterResource(R.drawable.ic_email), "example@email.com")
+    }
+}
+
+@Composable
+fun ContactRow(painter: Painter, contact: String, modifier: Modifier = Modifier) {
+    Row(modifier.padding(8.dp)) {
+        Icon(
+            painter = painter,
+            contentDescription = null,
+            tint = GreenDark,
+            modifier = Modifier.padding(end = 16.dp)
+        )
+        Text(
+            text = contact
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BusinessCardPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        BusinessCard()
     }
 }
--- 06_BusinessCard/app/src/main/java/com/ripalnakiya/businesscard/ui/theme/Color.kt
+++ 06_BusinessCard/app/src/main/java/com/ripalnakiya/businesscard/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val GreenLight = Color(0xFFD5F6D5)
+val GreenDark = Color(0xFF308132)
+
+val BlueExtraDark = Color(0xFF232F75)
