e5c774721db7ded29f3da9a1a950a05edb678185
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2026-09-13 01:43:49 +0530

    07 Dice Roller
    functional app

--- 07_DiceRoller/app/src/main/java/com/ripalnakiya/diceroller/MainActivity.kt
+++ 07_DiceRoller/app/src/main/java/com/ripalnakiya/diceroller/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ripalnakiya.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiceRollerTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DiceImageAndButton(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +43,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DiceImageAndButton(modifier: Modifier = Modifier) {
+    // A composable that reads observable Compose state can be recomposed when that state changes.
+    var result by remember { mutableIntStateOf(1) }
+
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = result.toString()
+        )
+
+        Spacer(Modifier.height(40.dp))
+
+        Button(onClick = { result = (1..6).random() }) {
+            Text("Roll")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun DiceRollerPreview() {
     DiceRollerTheme {
-        Greeting("Android")
+        DiceImageAndButton()
     }
 }
