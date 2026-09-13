65739809ae4281420ff671a8c8c94a597e703422
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2026-09-13 17:12:52 +0530

    08 Tip Time
    set keyboard action button

--- 08_TipTime/app/src/main/java/com/ripalnakiya/tiptime/MainActivity.kt
+++ 08_TipTime/app/src/main/java/com/ripalnakiya/tiptime/MainActivity.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -77,12 +78,20 @@ fun TipTimeLayout(modifier: Modifier = Modifier) {
             label = R.string.bill_amount,
             value = amountInput,
             onValueChanged = { amountInput = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next,
+            ),
             modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
         )
         EditNumberField(
             label = R.string.how_was_the_service,
             value = tipInput,
             onValueChanged = { tipInput = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done,
+            ),
             modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
         )
         Text(
@@ -98,6 +107,7 @@ fun EditNumberField(
     @StringRes label: Int,
     value: String,
     onValueChanged: (String) -> Unit,
+    keyboardOptions: KeyboardOptions,
     modifier: Modifier = Modifier
 ) {
     TextField(
@@ -105,7 +115,7 @@ fun EditNumberField(
         value = value,
         onValueChange = onValueChanged,
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = keyboardOptions,
         modifier = modifier,
     )
 }
