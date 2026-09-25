018829a93d47b5fccc3c80cf30853b303352fb84
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2026-09-25 16:15:11 +0530

    16 LunchTray
    Starter Code

---
+++ 16_LunchTray/app/src/main/java/com/ripalnakiya/lunchtray/ui/EntreeMenuScreen.kt
@@ -0,0 +1,44 @@
+package com.ripalnakiya.lunchtray.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.ripalnakiya.lunchtray.R
+import com.ripalnakiya.lunchtray.datasource.DataSource
+import com.ripalnakiya.lunchtray.model.MenuItem
+import com.ripalnakiya.lunchtray.model.MenuItem.EntreeItem
+
+@Composable
+fun EntreeMenuScreen(
+    options: List<EntreeItem>,
+    onCancelButtonClicked: () -> Unit,
+    onNextButtonClicked: () -> Unit,
+    onSelectionChanged: (EntreeItem) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    BaseMenuScreen(
+        options = options,
+        onCancelButtonClicked = onCancelButtonClicked,
+        onNextButtonClicked = onNextButtonClicked,
+        onSelectionChanged = onSelectionChanged as (MenuItem) -> Unit,
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun EntreeMenuPreview(){
+    EntreeMenuScreen(
+        options = DataSource.entreeMenuItems,
+        onCancelButtonClicked = {},
+        onNextButtonClicked = {},
+        onSelectionChanged = {},
+        modifier = Modifier
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .verticalScroll(rememberScrollState())
+    )
+}
