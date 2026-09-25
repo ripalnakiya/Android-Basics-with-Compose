20a7fc743f46058ac5022fe48ea43db209f9d653
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2026-09-25 16:39:01 +0530

    02 Happy Birthday
    add README

---
+++ 02_HappyBirthday/README.md
@@ -0,0 +1,6 @@
+# Happy Birthday
+
+- Learnt about accessing app resources in Compose (strings, drawables, dimens etc.)
+- Used Box Composable to overlap Image and Text Composables
+
+<img src="app_image_1.png" alt="app_image_1" width="250"/>
--- 02_HappyBirthday/app/src/main/java/com/ripalnakiya/happybirthday/MainActivity.kt
+++ 02_HappyBirthday/app/src/main/java/com/ripalnakiya/happybirthday/MainActivity.kt
@@ -89,6 +89,6 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        GreetingImage(message = "Happy Birthday Pengu!", from = "From Kanu")
+        GreetingImage(message = "Happy Birthday Abc!", from = "From Xyz")
     }
 }
